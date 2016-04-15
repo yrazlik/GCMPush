778a4b73dc3d6888f4ad4df4b7295b61f39be79b
Yasin Razlik <yrazlik@ebay.com>
2016-04-15 11:23:59 +0300

    Play services check

--- app/src/main/java/com/yrazlik/gcmpush/MainActivity.java
+++ app/src/main/java/com/yrazlik/gcmpush/MainActivity.java
@@ -3,7 +3,10 @@ package com.yrazlik.gcmpush;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.yrazlik.gcmpush.client.MyGCMRegistrationListenerService;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        registerGCM();
+
+        if(checkPlayServices()) {
+            registerGCM();
+        }
     }
 
     private void registerGCM(){
         Intent intent = new Intent(this, MyGCMRegistrationListenerService.class);
         startService(intent);
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, 9000).show();//9000 is the play services resolution request
+            } else {
+                Log.i("PLAY_SERVICES", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
